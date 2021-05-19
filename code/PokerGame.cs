eb413195b2db6b05ee43a29de6a9406b53922648
Alex <alex@gu3.me>
2021-05-19 22:57:34 +0100

    Start again from scratch (Pain Day)

--- code/PokerGame.cs
+++ code/PokerGame.cs
@@ -1,26 +1,7 @@
-﻿
-using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using Sandbox;
 
-/// <summary>
-/// You don't need to put things in a namespace, but it doesn't hurt.
-/// </summary>
-namespace MinimalExample
+namespace Poker
 {
-
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
-	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// 
-	/// Your game needs to be registered (using [Library] here) with the same name 
-	/// as your game addon. If it isn't then we won't be able to find it.
-	/// </summary>
 	[Library( "poker" )]
 	public partial class PokerGame : Sandbox.Game
 	{
@@ -29,11 +10,6 @@ namespace MinimalExample
 			if ( IsServer )
 			{
 				Log.Info( "My Gamemode Has Created Serverside!" );
-
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
 				new MinimalHudEntity();
 			}
 
@@ -42,10 +18,15 @@ namespace MinimalExample
 				Log.Info( "My Gamemode Has Created Clientside!" );
 			}
 		}
-
-		public override Player CreatePlayer()
+		
+		public override void ClientJoined( Client client )
 		{
-			return new MinimalPlayer();
+			base.ClientJoined( client );
+
+			var player = new MinimalPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
 		}
 	}
 
